daaf683da3df43c60c44afb009b7392507a80818
jandykane <jandykane@gmail.com>
2025-06-30 15:12:05 +0000

    Update callbackURL to use Vercel domain

--- api/_passport.js
+++ api/_passport.js
@@ -7,8 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL:
-        "https://99606ddb-4544-428d-8c08-5f136c36e911-00-3f0t3oeda3a5k.janeway.replit.dev/api/auth/callback",
+      callbackURL: "https://mosslog.vercel.app/api/auth/callback",
     },
     (accessToken, refreshToken, profile, done) => {
       return done(null, profile);
